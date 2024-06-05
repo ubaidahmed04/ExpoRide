2b2f4e6278f9b7313f75c325402dbe80364ee3f5
ubaid Dev <obaidkhanzada2004@gmail.com>
2024-06-05 15:36:52 +0500

    fix signup page style

--- app/index.tsx
+++ app/index.tsx
@@ -5,9 +5,12 @@ import {
 	Platform,
 	StyleSheet,
 	TextInput,
-	TouchableOpacity
+	TouchableOpacity,
+	View,
+	Image
 } from 'react-native';
 import { useAuth } from './authContext';
+import { EvilIcons, FontAwesome6, Octicons } from '@expo/vector-icons';
 
 const Page = () => {
 	const [username, setUsername] = useState('admin');
@@ -23,21 +26,41 @@ const Page = () => {
 			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
 			style={styles.container}
 		>
-			<Text style={styles.header}>My Epic App</Text>
+			<View>
+			<Image
+            source={{
+              uri: "https://static.vecteezy.com/system/resources/previews/006/960/543/original/electric-bike-motorcycle-and-route-icon-on-white-free-vector.jpg", 
+            }}
+            style={styles.avatar}
+          />
+			</View>
+			<Text style={styles.header}>Let's Get Started!
+			</Text>
+			<Text style={styles.headerBottom}>create an account to get all features </Text>
+
+		    <View style={styles.inputContainer}>
+			<FontAwesome6 name="user" size={20} color="#175E96" style={styles.icon} />
 			<TextInput
 				autoCapitalize="none"
 				placeholder="admin"
+				placeholderTextColor="#175E96"
 				value={username}
 				onChangeText={setUsername}
 				style={styles.inputField}
-			/>
+				/>
+				</View>
+		    <View style={styles.inputContainer}>
+			<Octicons name="unlock" size={20} color="#175E96" style={styles.icon} />
 			<TextInput
 				placeholder="password"
+				placeholderTextColor="#175E96"
 				value={password}
-				onChangeText={setPassword}
 				secureTextEntry
+				onChangeText={setPassword}
 				style={styles.inputField}
-			/>
+				/>
+				</View>
+			
 
 			<TouchableOpacity onPress={onSignInPress} style={styles.button}>
 				<Text style={{ color: '#fff' }}>Sign in</Text>
@@ -49,29 +72,53 @@ const Page = () => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		padding: 20,
-		paddingHorizontal: '20%',
-		justifyContent: 'center'
+		// padding: 20,
+		paddingHorizontal: '10%',
+		justifyContent: 'center',
+	},
+	avatar:{
+		width:"100%",
+		height:150,
+		objectFit:"contain"
 	},
 	header: {
 		fontSize: 30,
 		textAlign: 'center',
-		marginBottom: 40
+		marginTop: 40,
+	},
+	headerBottom:{
+		fontSize:14,
+		color:"#c3c3c3",
+		marginBottom: 30,
+		textAlign:"center",
+		paddingVertical:10,
+
+	},
+	icon:{
+			marginRight: 15,
+	},
+	inputContainer:{
+		flexDirection: 'row',
+		alignItems: 'center',
+		borderWidth: 1,
+		padding: 10,
+		marginVertical: 6,
+		height: 50,
+		borderColor: '#175E96',
+		borderRadius: 10,
+		color:"#175E96",
 	},
 	inputField: {
-		marginVertical: 4,
-		height: 50,
-		borderWidth: 1,
-		borderColor: '#ccc',
-		borderRadius: 4,
-		padding: 10
+		// flex: 1,
+    	fontSize: 16,
 	},
 	button: {
-		marginVertical: 15,
+		marginVertical: 25,
+		// marginHorizontal:50,
 		alignItems: 'center',
-		backgroundColor: '#111233',
+		backgroundColor: '#53A5F8',
 		padding: 12,
-		borderRadius: 4
+		borderRadius: 10
 	}
 });
 export default Page;
